6b4f7d3930c8f90f89204fc3fa4666b0af5d20fe
Nick <delta2888@gmail.com>
2023-08-10 15:34:14 +1000

    feat: add nextui components & nextthemes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import '~/styles/globals.css';
 import { type Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import { Providers } from '~/components/Providers';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -15,8 +17,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
