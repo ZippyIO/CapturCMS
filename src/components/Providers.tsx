5338d7596762e9c4e6250b2b25f4535075b626f1
Nick <delta2888@gmail.com>
2023-08-11 11:04:33 +1000

    build: remove nextui in favour of shadcn/ui & customs

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,11 @@
 'use client';
 
-import { NextUIProvider } from '@nextui-org/system';
 import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark">
-        {children}
-      </ThemeProvider>
-    </NextUIProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      {children}
+    </ThemeProvider>
   );
 }
