3b36502392f46a40dd64ff3a55e10947181b4397
Nick <delta2888@gmail.com>
2023-08-12 17:56:59 +1000

    feat: add Dashboard Collections & Settings Pages

---
+++ src/app/dashboard/collections/page.tsx
@@ -0,0 +1,9 @@
+const Collections = () => {
+  return (
+    <main className="flex flex-col items-center justify-between p-8">
+      <p>Collections</p>
+    </main>
+  );
+};
+
+export default Collections;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,9 @@
+const Settings = () => {
+  return (
+    <main className="flex flex-col items-center justify-between p-8">
+      <p>Settings</p>
+    </main>
+  );
+};
+
+export default Settings;
