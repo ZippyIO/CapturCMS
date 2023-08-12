dee08ae4d0187183fc094b58eb128c9f1cc540ea
Nick <ZippyIO@users.noreply.github.com>
2023-08-12 22:43:33 +1000

    feat: add /api/collections route handler

---
+++ src/app/api/collections/route.ts
@@ -0,0 +1,28 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse, type NextRequest } from 'next/server';
+import { z } from 'zod';
+import db from '~/lib/db';
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = await getToken({ req });
+
+    if (!token?.sub) {
+      return new Response('Unauthorized', { status: 401 });
+    }
+
+    const userId = z.string().min(1).parse(token.sub);
+
+    const collections = await db.imageCollection.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return NextResponse.json(collections, { status: 200 });
+  } catch (error) {
+    return new Response('Could not fetch Image Collections, please try again later', {
+      status: 500,
+    });
+  }
+}
