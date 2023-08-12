45be9ca53d53479637db7b6b71bb30b74cec6b6b
Nick <delta2888@gmail.com>
2023-08-12 14:55:10 +1000

    feat: add uploadthing component wrappers

---
+++ src/components/uploadthing/UploadButton.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { UploadButton } from '@uploadthing/react';
+
+import { type FileRouter } from '~/app/api/uploadthing/core';
+
+export default UploadButton<FileRouter>;
---
+++ src/components/uploadthing/UploadDropzone.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { UploadDropzone } from '@uploadthing/react';
+
+import { type FileRouter } from '~/app/api/uploadthing/core';
+
+export default UploadDropzone<FileRouter>;
