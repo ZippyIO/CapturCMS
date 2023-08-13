d00f2ba333a2d60c1e26bdab65994e90240c18b0
Nick <delta2888@gmail.com>
2023-08-13 13:50:52 +1000

    feat: add image update validator & type

--- src/lib/validators/image.ts
+++ src/lib/validators/image.ts
@@ -11,5 +11,13 @@ export const ImageValidator = z.object({
   fileUrl: z.string(),
 });
 
+export const ImageUpdateValidator = z.object({
+  id: z.string().nonempty(),
+  collectionId: z.string().optional().nullable(),
+  name: z.string().max(128).optional().nullable(),
+  description: z.string().optional().nullable(),
+});
+
 export type Image = z.infer<typeof ImageValidator>;
 export type CreateImagePayload = Omit<z.infer<typeof ImageValidator>, 'userId' | 'collectionId'>;
+export type UpdateImagePayload = z.infer<typeof ImageUpdateValidator>;
