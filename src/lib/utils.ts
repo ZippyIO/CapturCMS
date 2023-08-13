8669693ca69be749f1c95abc5a817825f480bb8d
Nick <delta2888@gmail.com>
2023-08-13 17:05:08 +1000

    feat: add getBaseUrl function for fetch requests

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,9 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function getBaseUrl() {
+  if (typeof window !== 'undefined') return '';
+  const vc = process.env.VERCEL_URL;
+  if (vc) return `https://${vc}`;
+  return 'http://localhost:3000';
 }
