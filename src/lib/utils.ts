fd80c6740660c244a60076b09e8bda9294cc6f81
Nick <delta2888@gmail.com>
2023-08-13 17:05:24 +1000

    feat: add coerceBoolean function with zod

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
+
 export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function getBaseUrl() {
   if (typeof window !== 'undefined') return '';
   const vc = process.env.VERCEL_URL;
   if (vc) return `https://${vc}`;
   return 'http://localhost:3000';
 }
+
+export const coerceBoolean = z
+  .enum(['0', '1', 'true', 'false'])
+  .catch('false')
+  .transform((value) => value == 'true' || value == '1');
