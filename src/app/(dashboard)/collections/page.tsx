c770923c75703344d0a4616a0601ea7ad25ec76b
Nick <delta2888@gmail.com>
2023-08-15 13:07:43 +1000

    feat: add CollectionCard
    - Display a collection with its header image & info

--- src/app/(dashboard)/collections/page.tsx
+++ src/app/(dashboard)/collections/page.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link';
-
-import { Button } from '~/components/ui/Button';
+import CollectionCard from '~/components/shared/CollectionCard';
 import { getAllImageCollections } from '~/server/image-collection';
 
 const Collections = async () => {
@@ -10,30 +8,17 @@ const Collections = async () => {
     <main className="flex flex-col items-center justify-center gap-2 p-8">
       <div className="flex flex-wrap items-center justify-center gap-2">
         {collections?.map((collection) => (
-          <div
+          <CollectionCard
             key={collection.id}
-            className="relative flex h-[500px] w-[300px] flex-col items-center justify-between rounded-md bg-zinc-900"
-          >
-            {collection.images[0] && (
-              <img
-                src={collection.images[0].fileUrl}
-                alt=""
-                className="h-[500px] w-[300px] rounded-md object-cover"
-              />
-            )}
-            <Button className="absolute right-4 top-4 bg-zinc-900/50 transition-transform hover:bg-zinc-950/50 active:scale-95">
-              <Link href={`/collection/${collection.id}`} prefetch={false} className="">
-                Open
-              </Link>
-            </Button>
-            <div className="absolute bottom-0 w-full rounded-b-md bg-zinc-900/70 py-1 pl-2">
-              <p className="text-xl font-medium">{collection.name}</p>
-            </div>
-          </div>
+            id={collection.id}
+            href={collection.images[0]?.fileUrl}
+            name={collection.name}
+          />
         ))}
       </div>
     </main>
   );
 };
 
+export const dynamic = 'force-dynamic';
 export default Collections;
---
+++ src/components/shared/CollectionCard.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Link from 'next/link';
+
+import { Button } from '~/components/ui/Button';
+
+interface Props {
+  id: string;
+  href?: string;
+  name: string;
+}
+
+const CollectionCard = ({ id, href, name }: Props) => {
+  return (
+    <div
+      key={id}
+      className="relative flex h-[500px] w-[300px] flex-col items-center justify-between rounded-md bg-zinc-900"
+    >
+      {href && <img src={href} alt="" className="h-[500px] w-[300px] rounded-md object-cover" />}
+      <Button className="absolute right-4 top-4 bg-zinc-900/50 transition-transform hover:bg-zinc-950/50 active:scale-95">
+        <Link href={`/collection/${id}`} prefetch={false} className="">
+          Open
+        </Link>
+      </Button>
+      <div className="absolute bottom-0 w-full rounded-b-md bg-zinc-900/70 py-1 pl-2">
+        <p className="text-xl font-medium">{name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CollectionCard;
