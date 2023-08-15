b75af90287f6e90567a6070689efb9a7282882eb
Nick <delta2888@gmail.com>
2023-08-15 12:44:00 +1000

    feat: add NewImage page

---
+++ src/app/(dashboard)/images/new/page.tsx
@@ -0,0 +1,14 @@
+import ImageForm from '~/components/ImageForm';
+import { getAllImageCollections } from '~/server/image-collection';
+
+const NewImage = async () => {
+  const collections = await getAllImageCollections();
+
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      {collections && <ImageForm collections={collections} />}
+    </main>
+  );
+};
+
+export default NewImage;
