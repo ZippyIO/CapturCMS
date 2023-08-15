21801ec7407301084c563bdfea07b9f1c185c697
Nick <delta2888@gmail.com>
2023-08-15 12:05:39 +1000

    feat: add ImageCollectionImageValidator & Type to image-collection validators

--- src/lib/validators/image-collection.ts
+++ src/lib/validators/image-collection.ts
@@ -1,9 +1,20 @@
 import { z } from 'zod';
 
+import { ImageValidator } from '~/lib/validators/image';
+
+export const ImageCollectionImageValidator = ImageValidator.extend({
+  id: z.string().optional(),
+  userId: z.string().optional().nullable(),
+  collectionId: z.string().optional().nullable(),
+  name: z.string().optional().nullable(),
+  description: z.string().nullable(),
+});
+
 export const ImageCollectionValidator = z.object({
   userId: z.string().min(1),
   name: z.string().min(1).max(128),
   description: z.string().optional(),
+  images: z.array(ImageCollectionImageValidator),
 });
 
 export const ImageCollectionUpdateValidator = z.object({
@@ -11,6 +22,7 @@ export const ImageCollectionUpdateValidator = z.object({
   collectionId: z.string().min(1),
 });
 
+export type ImageCollectionImage = z.infer<typeof ImageCollectionImageValidator>;
 export type ImageCollection = z.infer<typeof ImageCollectionValidator>;
 export type CreateImageCollectionPayload = Omit<z.infer<typeof ImageCollectionValidator>, 'userId'>;
 
