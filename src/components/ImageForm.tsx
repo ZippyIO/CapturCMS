6efa7b55446700d3b698d5a552b95e794b88a831
Nick <delta2888@gmail.com>
2023-08-15 12:43:52 +1000

    feat: use createImage server function in ImageForm instead of fetch + update layout

--- src/components/ImageForm.tsx
+++ src/components/ImageForm.tsx
@@ -11,6 +11,7 @@ import ReactCrop, { type Crop, type PixelCrop } from 'react-image-crop';
 import { type z } from 'zod';
 
 import { Button } from '~/components/ui/Button';
+import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel } from '~/components/ui/Form';
 import { Input } from '~/components/ui/Input';
 import {
@@ -24,6 +25,7 @@ import { Textarea } from '~/components/ui/Textarea';
 import useUploadThing from '~/hooks/useUploadThing';
 import { cropImage } from '~/lib/image';
 import { type CreateImagePayload, ImageValidator } from '~/lib/validators/image';
+import { createImage } from '~/server/image';
 
 const FormSchema = ImageValidator.pick({ name: true, description: true, collectionId: true });
 
@@ -82,124 +84,120 @@ const ImageForm = ({ collections }: Props) => {
   }
 
   async function onSubmit(values: FormValues) {
-    console.log('Form values', values);
     if (!imgFile) return;
 
-    const image = await uploadImage()
-      .then(async (res) => {
-        const file = res?.[0];
-        if (!file) return;
+    const image = await uploadImage().then(async (res) => {
+      const file = res?.[0];
+      if (!file) return;
 
-        console.log('Upload complete', file);
+      const payload: CreateImagePayload = {
+        collectionId: values.collectionId,
+        name: values.name?.length ?? 0 > 0 ? values.name : undefined,
+        description: values.description?.length ?? 0 > 0 ? values.description : undefined,
+        fileName: file.name,
+        fileSize: file.size,
+        fileKey: file.key,
+        fileUrl: file.url,
+      };
 
-        const payload: CreateImagePayload = {
-          collectionId: values.collectionId,
-          name: values.name?.length ?? 0 > 0 ? values.name : undefined,
-          description: values.description?.length ?? 0 > 0 ? values.description : undefined,
-          fileName: file.name,
-          fileSize: file.size,
-          fileKey: file.key,
-          fileUrl: file.url,
-        };
+      const image = await createImage(payload);
 
-        const createImage = await fetch('/api/image/create', {
-          method: 'POST',
-          cache: 'no-cache',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(payload),
-        });
-
-        return createImage;
-      })
-      .catch((e) => {
-        console.error(e);
-      });
+      return image;
+    });
 
     return image;
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Name" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Textarea placeholder="Description" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="collectionId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Collection</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a Collection" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  {collections.map((collection) => (
-                    <SelectItem key={collection.id} value={collection.id}>
-                      {collection.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </FormItem>
-          )}
-        />
-        <FormLabel>File</FormLabel>
-        <Input
-          type="file"
-          placeholder="Name"
-          onChange={(e) => {
-            const file = e.target.files?.[0];
-            if (!file) return;
-            const reader = new FileReader();
-            reader.addEventListener('load', () => {
-              setImgSrc(reader.result?.toString() ?? '');
-              setImgFile(file);
-            });
-            reader.readAsDataURL(file);
-          }}
-        />
-        <div className="w-full max-w-md space-y-2">
-          {imgSrc && (
-            <ReactCrop
-              crop={crop}
-              disabled
-              onChange={(_, percent) => setCrop(percent)}
-              onComplete={(c) => setStoredCrop(c)}
-            >
-              <img ref={imageRef} src={imgSrc} alt="Crop me" />
-            </ReactCrop>
-          )}
-        </div>
-        <Button type="submit">{isUploading ? 'Uploading...' : 'Submit'}</Button>
-      </form>
-    </Form>
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Create Image</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-2">
+            <div className="flex w-full gap-2">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="w-3/5">
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input className="w-full" placeholder="Name" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="collectionId"
+                render={({ field }) => (
+                  <FormItem className="w-2/5">
+                    <FormLabel>Collection</FormLabel>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select a Collection" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {collections.map((collection) => (
+                          <SelectItem key={collection.id} value={collection.id}>
+                            {collection.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Description" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormLabel>File</FormLabel>
+            <Input
+              type="file"
+              placeholder="Name"
+              onChange={(e) => {
+                const file = e.target.files?.[0];
+                if (!file) return;
+                const reader = new FileReader();
+                reader.addEventListener('load', () => {
+                  setImgSrc(reader.result?.toString() ?? '');
+                  setImgFile(file);
+                });
+                reader.readAsDataURL(file);
+              }}
+            />
+            <div className="flex items-center justify-center">
+              {imgSrc && (
+                <ReactCrop
+                  crop={crop}
+                  disabled
+                  onChange={(_, percent) => setCrop(percent)}
+                  onComplete={(c) => setStoredCrop(c)}
+                  className="max-h-[400px]"
+                >
+                  <img ref={imageRef} src={imgSrc} alt="Crop me" className="rounded-md" />
+                </ReactCrop>
+              )}
+            </div>
+            <Button type="submit">{isUploading ? 'Uploading...' : 'Submit'}</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 
