098cce860c17bd4cd4d50f9aa7dc2d0082620045
Nick <delta2888@gmail.com>
2023-08-15 12:42:12 +1000

    feat: add createImage server function

--- src/server/image.ts
+++ src/server/image.ts
@@ -1,9 +1,11 @@
 'use server';
 
 import { getServerSession } from 'next-auth';
+import { z } from 'zod';
 
 import { nextAuthOptions } from '~/lib/auth';
 import db from '~/lib/db';
+import { type Image, ImageValidator } from '~/lib/validators/image';
 
 export async function getAllUserImages() {
   const session = await getServerSession(nextAuthOptions);
@@ -37,5 +39,39 @@ export async function getImage(id: string) {
     });
 
     return image;
+  }
+}
+
+export async function createImage(data: Omit<Image, 'userId'>) {
+  try {
+    const session = await getServerSession(nextAuthOptions);
+
+    if (!session) {
+      throw new Error('Unauthorized');
+    }
+
+    const { userId, collectionId, name, description, fileName, fileSize, fileKey, fileUrl } =
+      ImageValidator.parse({ userId: session.user.id, ...data });
+
+    const newImage = await db.image.create({
+      data: {
+        userId: userId,
+        collectionId: collectionId,
+        name: name,
+        description: description,
+        fileName: fileName,
+        fileSize: fileSize,
+        fileKey: fileKey,
+        fileUrl: fileUrl,
+      },
+    });
+
+    return newImage;
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      throw new Error(error.message);
+    }
+
+    throw new Error('Could not create Image, please try again later');
   }
 }
