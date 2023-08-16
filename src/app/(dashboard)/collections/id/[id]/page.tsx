e4a6086e86bc03335f6afb9ce409c46a353d13da
Nick <delta2888@gmail.com>
2023-08-16 11:02:34 +1000

    feat: add CollectionContainer with search filtering for image name

--- src/app/(dashboard)/collections/id/[id]/page.tsx
+++ src/app/(dashboard)/collections/id/[id]/page.tsx
@@ -1,4 +1,4 @@
-import ImageCard from '~/components/shared/ImageCard';
+import CollectionContainer from '~/components/collection/CollectionContainer';
 import { getImageCollection } from '~/server/image-collection';
 
 const Collection = async ({ params }: { params: { id: string } }) => {
@@ -6,21 +6,7 @@ const Collection = async ({ params }: { params: { id: string } }) => {
 
   return (
     <main className="flex flex-col items-center justify-between gap-2 p-8">
-      <div className="flex w-full flex-col items-center rounded-md bg-zinc-900/50 py-1.5">
-        <h2 className="text-2xl font-semibold text-zinc-300">{collection?.name}</h2>
-        <p className="text-sm font-light text-zinc-400">{collection?.description}</p>
-      </div>
-      <div className="columns-3 gap-2 space-y-2">
-        {collection?.images?.map((image) => (
-          <ImageCard
-            key={image.fileKey}
-            id={image.id}
-            href={image.fileUrl}
-            name={image.name ?? ''}
-            collectionName={collection.name}
-          />
-        ))}
-      </div>
+      {collection && <CollectionContainer collection={collection} />}
     </main>
   );
 };
---
+++ src/components/collection/CollectionContainer.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { type Image, type ImageCollection } from '@prisma/client';
+
+import { useMemo, useState } from 'react';
+
+import ImageCard from '~/components/shared/ImageCard';
+import { Input } from '~/components/ui/Input';
+
+interface Collection extends ImageCollection {
+  images: Image[];
+}
+
+interface Props {
+  collection: Collection;
+}
+
+const CollectionContainer = ({ collection }: Props) => {
+  const [search, setSearch] = useState('');
+
+  const filteredImages = useMemo(() => {
+    if (!search) return collection?.images;
+
+    return collection?.images.filter(
+      (image) => image.name?.toLowerCase().includes(search.toLowerCase()),
+    );
+  }, [collection?.images, search]);
+
+  return (
+    <>
+      <div className="flex w-full flex-col items-center rounded-md bg-zinc-900/50 py-1.5">
+        <h2 className="text-2xl font-semibold text-zinc-300">{collection?.name}</h2>
+        <p className="text-sm font-light text-zinc-400">{collection?.description}</p>
+        <Input
+          name="description"
+          placeholder="Search"
+          type="text"
+          className="mt-2 w-1/2"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="columns-3 gap-2 space-y-2">
+        {filteredImages?.map((image) => (
+          <ImageCard
+            key={image.fileKey}
+            id={image.id}
+            href={image.fileUrl}
+            name={image.name ?? ''}
+            collectionName={collection.name}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CollectionContainer;
