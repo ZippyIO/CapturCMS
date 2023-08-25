d6a8a13f3d2e8186c0e49896ea09e31dc68f18cc
Nick <delta2888@gmail.com>
2023-08-25 11:44:21 +1000

    feat: move dashboard routes into /dashboard

---
+++ src/app/(portfolio)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="bg rounded-md bg-zinc-900/50 p-2 text-center"></div>
+    </main>
+  );
+};
+
+export default Home;
